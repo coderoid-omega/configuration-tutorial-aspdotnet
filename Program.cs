5f9e2ff24430a5788335ea432073ba49baab88fc
harshascensive <87983457+harshascensive@users.noreply.github.com>
2023-08-22 18:02:36 +0530

    basic way to get value from appsettings.json

--- Program.cs
+++ Program.cs
@@ -4,6 +4,21 @@ var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseRouting();
+
+//Adding endpoints
+app.UseEndpoints(endpoints =>
+{
+    endpoints.Map("/", async context =>
+    {
+        //way 1
+        await context.Response.WriteAsync(app.Configuration["MyKey"].ToString() + "\n");
+        //way 2, using get value
+        await context.Response.WriteAsync(app.Configuration.GetValue<string>("MyKey") + "\n");
+        //way 3 using getvalue with default value if key not found
+        await context.Response.WriteAsync(app.Configuration.GetValue<int>("NewKey", 3).ToString());
+    });
+});
+
 app.MapControllers();
 
 
